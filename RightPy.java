a465e4d85997aec4d993a1716d371c7aa9e9c712
Nandha Visvanathan <nandhavisvanathan25@gmail.com>
2025-07-02 16:18:36 +0530

    next six

---
+++ RightPy.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class RightPy{
+  public static void main(String[] args){
+    for(int i=0;i<5;i++){
+      for(int j=0;j<=i;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
